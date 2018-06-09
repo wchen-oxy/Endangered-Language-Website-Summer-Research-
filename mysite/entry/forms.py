be93c1696e8d815bdf40cf383b71d58722fb08d4
Unknown <blackjamx5@hotmail.com>
2018-06-08 21:55:32 -0700

    Added Everything

---
+++ mysite/entry/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class searchform(forms.Form):
+    search_date = forms.
---
+++ mysite/entry/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import entry
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'entry/index.html')
+
+def entry_list(request):
+    entrys = entry.objects.all()
+    return render(request, 'entry/entry.html', {"entrys" : entrys})
+
+   
