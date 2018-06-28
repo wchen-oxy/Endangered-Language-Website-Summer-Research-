8a970a6347c6de4efe376ada22adf3cd9c9d93e5
Unknown <blackjamx5@hotmail.com>
2018-06-27 20:45:22 -0700

    Adjusted Results
    -exact results return just the exact results
    -inexact results return related results
    -fixed "No Term Found"
    -formatted html

--- mysite/entry/views.py
+++ mysite/entry/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import entry
 from django.http import HttpResponse
+from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank
+from django.conf.urls import include
+from django.conf.urls import url
+from django.http import HttpRequest, HttpResponseRedirect as redirect
 
 
 # Create your views here.
@@ -9,21 +13,21 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'index.html')
 
-def search(request):
-    if 'q' in request.GET and request.GET['q']:
-        q = request.GET['q']
-        entrys = entry.objects.filter(bhutia__icontains=q)
-        if entrys:
-            return render(request, 'entry/entry_list.html',
-                          {'entrys': entrys, 'query': q})
-        else:
-            error = True
-            return render(request, 'entry/entry_list.html', {'error': error})
-
-    else:
-        error = True
-        return render(request, 'entry/entry_list.html', {'error': error})
+# def search(request):
+#     #checks if there is a match at all
+#     if 'q' in request.GET and request.GET['q']:
+#         q = request.GET['q']
+#         entrys = entry.objects.filter(bhutia__icontains=q)
+#         if entrys:
+#             return render(request, 'entry/entry_list.html',
+#                           {'entrys': entrys, 'query': q})
+#         else:
+#             error = True
+#             return render(request, 'entry/entry_list.html', {'error': error})
 #
+#     else:
+#         error = True
+#         return render(request, 'entry/entry_list.html', {'error': error})
 #
 # def search(request):
 #     error = False
@@ -32,6 +36,26 @@ def search(request):
 #         if not q:
 #             error = True
 #         else:
-#             books = entry.objects.filter(bhutia__icontains=q)
-#             return render(request, 'entry/entry_list.html', {'books': books, 'query': q})
-#     return render(request, 'index.html', {'error': error})
+#             entrys = entry.objects.filter(bhutia__icontains=q)
+#             length = len(entrys)
+#             #run comparisons
+#             if length != 1:
+#
+#                 return render(request, 'entry/entry_list.html', {'entrys': entrys, 'query': q})
+#             return render(request,  'entry/entry_list.html', {'entrys': entrys, 'query': q})
+#         return render(request, 'entry/entry_list.html', {'error': error})
+
+def search(request):
+    error = False
+    if 'q' in request.GET:
+        q = request.GET['q']
+        if not q:
+            error = True
+        else:
+            exact_entry = entry.objects.filter(bhutia__iexact=q)
+            entrys = entry.objects.filter(bhutia__icontains=q)
+            
+            #run comparisons
+
+            return render(request,  'entry/entry_list.html', {'entrys': entrys, 'query': exact_entry})
+        return render(request, 'entry/entry_list.html', {'error': error})
