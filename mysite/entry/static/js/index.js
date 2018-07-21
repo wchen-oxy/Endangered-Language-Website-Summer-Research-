432ee292eac26424f05a627b19fe5fe28878b8aa
Unknown <blackjamx5@hotmail.com>
2018-07-20 20:25:03 -0700

    More Stuff I don't remember

--- mysite/entry/static/js/index.js
+++ mysite/entry/static/js/index.js
@@ -1,16 +1,42 @@
 
+//add listeners
 document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector('select[name="translation"]').onchange=changeEventHandler;
+  document.querySelector('select[name="translation"]').onchange=changeEventHandler;
 },false);
 
-function changeEventHandler(event) {
-    // You can use “this” to refer to the selected element.
+//code for things that need to be run each time the page is run
+window.onload = function() {
 
+  var selected = document.createAttribute("selected");
+  if (localStorage.getItem("storageName")) {
+    var thing = localStorage.getItem("storageName");
+    var element = document.getElementById(thing);
 
-    if(event.target.value == "engToBhut") document.myform.setAttribute("action", "/entry/english_bhutia/");
+    if (thing == "be") {
+      document.myform.setAttribute("action","/entry/bhutia_english/");};
+      if (thing == "eb") {document.myform.setAttribute("action", "/entry/english_bhutia/");
+      element.setAttribute("selected", "selected");};
+      if (thing == "tb") {document.myform.setAttribute("action", "/entry/tibetan_bhutia/");
+      element.setAttribute("selected", "selected");
+    };}
+    console.log(document.getElementById("translation"));
 
-    if (event.target.value == "bhutToEng") document.myform.setAttribute("action", "/entry/bhutia_english/");
+  }
 
-    if (event.target.value == "tibToBhut")   document.myform.setAttribute("action", "/entry/tibetan_bhutia/");
+  //code that is only run when a change is detected
+  function changeEventHandler(event) {
+    if (event.target.value == "bhutToEng") {
+      document.myform.setAttribute("action", "/entry/bhutia_english/");
+      localStorage.setItem("storageName", "be");
 
+    }
+    if(event.target.value == "engToBhut") {
+      document.myform.setAttribute("action", "/entry/english_bhutia/");
+      localStorage.setItem("storageName", "eb");
+
+    }
+
+    if (event.target.value == "tibToBhut")  { document.myform.setAttribute("action", "/entry/tibetan_bhutia/");
+    localStorage.setItem("storageName", "tb");
+  }
 }
